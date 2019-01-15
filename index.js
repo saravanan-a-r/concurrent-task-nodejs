0ca3b13e1c0e57b86ab4de16bae2e8e8a169bd87
System Administrator <Saravanan A R>
2019-01-16 01:51:32 +0530

    Builder design pattern mode object creation added

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ function delayJobs(jobName, seconds) {
     });
 }
 
+function cbPromise() {
+    return new Promise( (resolve, reject) => {
+        console.log("Complete");
+        resolve();
+    });
+}
+
+function errorCb(err) {
+    console.log(err);
+}
+
 var tasks = [
     delayJobs("Job - 1", 2),
     delayJobs("Job - 2", 5),
@@ -22,4 +33,4 @@ var tasks = [
     delayJobs("Job - 11", 5),
 ];
 
-new TaskQueue(tasks, 2).run();
+new TaskQueue(tasks, 2, cbPromise, errorCb).run();
--- task-queue.js
+++ task-queue.js
@@ -3,10 +3,34 @@ class TaskQueue {
     /*
         @taskQueue -> Array of promised actions
         @taskCount -> Maximum number of tasks to execute at a time
+        @processCallback -> A callback which will get invoke whenever a promise resolve
+        @errorCallback -> A callback which will get invoke whenever a promise reject
     */
-    constructor(taskQueue, taskCount) {
+    constructor(taskQueue, taskCount, processCallback, errorCallback) {
         this.taskQueue = taskQueue;
         this.taskCount = taskCount; 
+        this.processCallback = processCallback;
+        this.errorCallback = errorCallback;
+    }
+
+    setTaskQueue(taskQueue) {
+        this.taskQueue = taskQueue;
+        return this;
+    }
+
+    setLimit(taskCount) {
+        this.taskCount = taskCount;
+        return this;
+    }
+
+    setProcessCallback(processCallback) {
+        this.processCallback = processCallback;
+        return this;
+    }
+
+    setErrorCallback(errorCallback) {
+        this.errorCallback = errorCallback;
+        return this;
     }
 
     run() {
@@ -16,13 +40,13 @@ class TaskQueue {
         while(this.taskCount && this.taskCount-- && taskLen) {
             var task = taskQueue.shift();
             /* use finally here to avoid code repeatation */
-            task.then( (result) => {
-                console.log(result);
+            task.then( async (result) => {
                 this.taskCount++;
+                await this.processCallback();
                 this.run();
-            }).catch( (err) => {
-                console.log(err);
+            }).catch( async (err) => {
                 this.taskCount++;
+                this.errorCallback(err);
                 this.run();
             });
         }
